4c6ea9935a1ec92143a2a812f777c99ae6782d45
MarcinK <marcin.kulbicki@gmail.com>
2023-09-01 10:28:51 +0200

    webpack base config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.min.js',
+    },
+    module: {
+        rules: [],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
