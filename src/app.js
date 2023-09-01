25a6c13d4cf68df39d2769950b586fa790395dc9
MarcinK <marcin.kulbicki@gmail.com>
2023-09-01 10:46:01 +0200

    babel config for react

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-// const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector('#root'));
 
-// root.render(<h1>Działa</h1>);
+root.render(<h1>Działa</h1>);
+
+console.log('first')
