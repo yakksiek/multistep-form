b6aee93cd5c7757755ebf738022a0089438db760
MarcinK <marcin.kulbicki@gmail.com>
2023-09-11 00:26:18 +0200

    reset styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Reset } from 'styled-reset';
 
 import GlobalFonts from './fonts';
 import GlobalStyles from './GlobalStyles';
@@ -9,6 +10,7 @@ import App from './components/App';
 const root = createRoot(document.querySelector('#root'));
 root.render(
     <>
+        <Reset />
         <GlobalFonts />
         <GlobalStyles />
         <App />
