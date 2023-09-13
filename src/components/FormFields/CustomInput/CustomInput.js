8ca3c3ddc4832fe8a88566baf088326a88172edb
MarcinK <marcin.kulbicki@gmail.com>
2023-09-13 10:54:39 +0200

    fix render customInput styles bug

--- src/components/FormFields/CustomInput/CustomInput.js
+++ src/components/FormFields/CustomInput/CustomInput.js
@@ -11,25 +11,6 @@ import StyledButton from '../../Button/Button.styled';
 import IconWrapper from '../../IconWrapper';
 import Checkbox from '../Checkbox/Checkbox';
 
-const StyledInputWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    gap: 10px;
-    align-items: center;
-    ${(props) =>
-        props.type === 'file' ||
-        (props.type === 'checkbox' &&
-            css`
-                position: relative;
-                display: flex;
-                align-items: center;
-                border-radius: var(--outer-radius);
-                padding: var(--element-padding);
-                box-shadow: var(--box-shadow-convex);
-                height: 55px;
-            `)}
-`;
-
 function CustomInput({ children, data }) {
     const { type, value, name, label, id, groupName, accept, onChange, error, deleteButton, handleClick } = data;
 
@@ -68,5 +49,23 @@ function CustomInput({ children, data }) {
         </Wrapper>
     );
 }
+
+const StyledInputWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    align-items: center;
+    ${(props) =>
+        (props.type === 'file' || props.type === 'checkbox') &&
+        css`
+            position: relative;
+            display: flex;
+            align-items: center;
+            border-radius: var(--outer-radius);
+            padding: var(--element-padding);
+            box-shadow: var(--box-shadow-convex);
+            height: 55px;
+        `}
+`;
 
 export default CustomInput;
