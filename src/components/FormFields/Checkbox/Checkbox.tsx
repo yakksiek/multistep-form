d223dfb0f74a688a6b67e8dc917be957e216a586
MarcinK <marcin.kulbicki@gmail.com>
2024-04-03 13:44:15 +0200

    feat(checkbox): add types

---
+++ src/components/FormFields/Checkbox/Checkbox.tsx
@@ -0,0 +1,38 @@
+import { FormField } from 'types/formFieldData.interfaces';
+import Wrapper from '../../Wrapper';
+import { StyledInputWrapper } from '../CustomInput/CustomInput.styled';
+import Label from '../Label';
+import { StyledCheckbox, StyledCircle, StyledSlider, StyledToggleContainer } from './Checkbox.styled';
+
+interface Props {
+    data: FormField;
+    value: boolean;
+}
+
+function Checkbox({ data, value }: Props) {
+    const { type, onChange, name, id, label } = data;
+
+    return (
+        <Wrapper variant="flex-column">
+            <StyledInputWrapper type={type}>
+                <Label htmlFor={id} type={type}>
+                    {label}:
+                </Label>
+                <StyledToggleContainer>
+                    <StyledCheckbox
+                        type={type}
+                        checked={value}
+                        onChange={(e) => onChange && onChange(e, id)}
+                        name={name}
+                        id={id}
+                    />
+                    <StyledSlider>
+                        <StyledCircle checked={value} />
+                    </StyledSlider>
+                </StyledToggleContainer>
+            </StyledInputWrapper>
+        </Wrapper>
+    );
+}
+
+export default Checkbox;
