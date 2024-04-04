0f5a2d9edf33a34a6db9ac08cd98fc2b8f62d532
MarcinK <marcin.kulbicki@gmail.com>
2024-04-04 16:34:40 +0200

    feat(ContextProviders) add types for contexts

--- src/context/ImageUploaderContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const ImageUploaderContext = createContext();
-
-export function useImageUploaderContext() {
-    return useContext(ImageUploaderContext);
-}
